103dd2d234e791cf82fc604ba8bf870a7559ad64
ahmetbekan <68969636+ahmetbekan@users.noreply.github.com>
2020-09-26 16:31:43 -0500

    Create ansible-melodi.groovy

---
+++ ansible-melodi.groovy
@@ -0,0 +1,16 @@
+properties([
+    parameters([
+        string(defaultValue: '', description: 'Provide node IP', name: 'node', trim: true)
+        ])
+    ])
+
+node {
+    stage("Pull Repo"){
+        sh "rm -rf ansible-melodi && git clone https://github.com/ikambarov/ansible-melodi.git"
+    }
+
+    stage("Install Melodi"){
+        ansiblePlaybook become: true, colorized: true, credentialsId: 'Jenkins-Master', disableHostKeyChecking: true, inventory: "${params.node},", playbook: 'ansible-melodi/main.yml'
+    }
+   
+}
